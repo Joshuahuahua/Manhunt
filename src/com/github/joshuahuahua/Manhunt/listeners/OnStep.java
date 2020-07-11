e69976134de922c349a93e30d7453b9ada607278
Joshuahuahua <joshuahollander2001@gmail.com>
2020-07-11 19:58:43 +0800

    Create OnStep.java

---
+++ src/com/github/joshuahuahua/Manhunt/listeners/OnStep.java
@@ -0,0 +1,14 @@
+package com.github.joshuahuahua.Manhunt.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+
+public class OnStep implements Listener {
+
+    //@EventHandler(priority= EventPriority.HIGH)
+    @EventHandler
+    public void onPlayerStep(PlayerInteractEvent event) {
+    }
+}
